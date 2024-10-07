ff1ecfebd3e33f6efa443bfdae9333cdfee26c69
Matias DUBOIS <matias.dubois@edu.esiee-it.fr>
2024-10-07 15:10:38 +0200

    Modif Matias 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Modif Matias");
+        System.out.println("Modif Matias 2");
 
     System.out.println("COMMIT BOUCLE 3");
     }
