413016f7a727155e5aafaf6729b27e67df61a303
Matias DUBOIS <matias.dubois@edu.esiee-it.fr>
2024-10-07 13:51:55 +0200

    Modif MASTER

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("github");
+        System.out.println("Modif GIT");
+        System.out.println("Modif MASTER");
     }
 }
